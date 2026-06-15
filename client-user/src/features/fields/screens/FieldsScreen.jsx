4302019a930920a574f7908c49532772ab73f976
esantos2019254 <esantos-2019254@kinal.edu.gt>
2026-06-15 15:36:34 -0600

    kinal sports

---
+++ client-user/src/features/fields/screens/FieldsScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const FieldsScreen = () => {
+    return (
+        <View>
+            <Text>Fields Screen</Text>
+        </View>
+    )
+}
+
+export default FieldsScreen;
